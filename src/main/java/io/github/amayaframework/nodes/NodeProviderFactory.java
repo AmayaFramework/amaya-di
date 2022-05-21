86bcb0c5fb2c6ad532d58a239859b66b61dcff7c
RomanQed <gbakaldin@gmail.com>
2022-05-21 19:08:41 +0300

    Remove NodeProviderFactory combining ability

--- src/main/java/io/github/amayaframework/nodes/NodeProviderFactory.java
+++ src/main/java/io/github/amayaframework/nodes/NodeProviderFactory.java
@@ -9,37 +9,6 @@ import java.lang.reflect.Method;
  */
 public interface NodeProviderFactory {
     /**
-     * Combines two provider factories into one.
-     *
-     * @param left  factory to be executed first
-     * @param right factory to be executed second
-     * @return the result of the merger
-     */
-    static NodeProviderFactory combine(NodeProviderFactory left, NodeProviderFactory right) {
-        return new NodeProviderFactory() {
-            @Override
-            public NodeProvider<Class<?>> getTypeProvider() {
-                return NodeProvider.combine(left.getTypeProvider(), right.getTypeProvider());
-            }
-
-            @Override
-            public NodeProvider<Constructor<?>> getConstructorProvider(Class<?> clazz) {
-                return NodeProvider.combine(left.getConstructorProvider(clazz), right.getConstructorProvider(clazz));
-            }
-
-            @Override
-            public NodeProvider<Method> getMethodProvider(Class<?> clazz) {
-                return NodeProvider.combine(left.getMethodProvider(clazz), right.getMethodProvider(clazz));
-            }
-
-            @Override
-            public NodeProvider<Field> getFieldProvider(Class<?> clazz) {
-                return NodeProvider.combine(left.getFieldProvider(clazz), right.getFieldProvider(clazz));
-            }
-        };
-    }
-
-    /**
      * Creates a provider for types.
      *
      * @return the resulting provider
@@ -69,14 +38,4 @@ public interface NodeProviderFactory {
      * @return the resulting provider
      */
     NodeProvider<Field> getFieldProvider(Class<?> clazz);
-
-    /**
-     * Combines the current provider factory with the received one.
-     *
-     * @param next factory to combine
-     * @return the result of the merger
-     */
-    default NodeProviderFactory combine(NodeProviderFactory next) {
-        return combine(this, next);
-    }
 }
