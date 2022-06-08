12173007c3293405a63aba5e3fb748cc4efa4e6e
RomanQed <gbakaldin@gmail.com>
2022-06-08 04:21:38 +0300

    Make AsmMethodVisitor internal

--- src/main/java/io/github/amayaframework/di/transformers/AsmClassVisitor.java
+++ src/main/java/io/github/amayaframework/di/transformers/AsmClassVisitor.java
@@ -1,18 +1,25 @@
 package io.github.amayaframework.di.transformers;
 
+import com.github.romanqed.jeflect.lambdas.AsmUtil;
+import io.github.amayaframework.di.annotations.InjectPolicy;
 import io.github.amayaframework.di.containers.ProviderType;
-import io.github.amayaframework.di.types.InjectType;
-import io.github.amayaframework.di.types.SubTypeFactory;
+import io.github.amayaframework.di.types.*;
 import org.objectweb.asm.ClassVisitor;
 import org.objectweb.asm.MethodVisitor;
 import org.objectweb.asm.Opcodes;
+import org.objectweb.asm.Type;
 
+import java.lang.reflect.Method;
+import java.util.Collection;
 import java.util.Objects;
 
 import static com.github.romanqed.jeflect.lambdas.AsmUtil.EMPTY_DESCRIPTOR;
 import static com.github.romanqed.jeflect.lambdas.AsmUtil.INIT;
 
 class AsmClassVisitor extends ClassVisitor {
+    private static final String DESCRIPTOR = AsmUtil.formatDescriptor("L" + AsmUtil.OBJECT + ";", "I");
+    private static final String VALUE = "getValue";
+    private static final String SINGLETON = "getSingleton";
     private final InjectType type;
     private final SubTypeFactory factory;
     private final ProviderType provider;
@@ -32,8 +39,89 @@ class AsmClassVisitor extends ClassVisitor {
                                      String[] exceptions) {
         MethodVisitor visitor = super.visitMethod(access, name, descriptor, signature, exceptions);
         if (Objects.equals(name, INIT) && Objects.equals(descriptor, EMPTY_DESCRIPTOR)) {
-            return new AsmMethodVisitor(visitor, type, factory, provider);
+            return new AsmMethodVisitor(visitor);
         }
         return visitor;
+    }
+
+    class AsmMethodVisitor extends MethodVisitor {
+        private final Type owner;
+        private final Collection<InjectField> fields;
+        private final Collection<InjectMethod> methods;
+
+        AsmMethodVisitor(MethodVisitor visitor) {
+            super(Opcodes.ASM8, visitor);
+            this.owner = Type.getType(type.getTarget());
+            this.fields = type.getFields();
+            this.methods = type.getMethods();
+        }
+
+        @Override
+        public void visitInsn(int opcode) {
+            if (opcode == Opcodes.RETURN) {
+                fields.forEach(this::setField);
+                methods.forEach(this::callMethod);
+            }
+            super.visitInsn(opcode);
+        }
+
+        private void setField(InjectField field) {
+            super.visitVarInsn(Opcodes.ALOAD, 0);
+            // Body
+            loadArgument(field);
+            // Set field
+            super.visitFieldInsn(Opcodes.PUTFIELD,
+                    owner.getInternalName(),
+                    field.getName(),
+                    field.getType().getDescriptor());
+        }
+
+        private void callMethod(InjectMethod method) {
+            super.visitVarInsn(Opcodes.ALOAD, 0);
+            // Body
+            loadArgument(method);
+            // Invoke method
+            Class<?> methodOwner = method.getMethod().getDeclaringClass();
+            super.visitMethodInsn(Opcodes.INVOKEVIRTUAL,
+                    Type.getType(methodOwner).getInternalName(),
+                    method.getName(),
+                    method.getMethodType().getDescriptor(),
+                    methodOwner.isInterface());
+        }
+
+        private void loadArgument(InjectMember member) {
+            InjectPolicy policy = member.getPolicy();
+            // Prototype
+            if (policy == InjectPolicy.PROTOTYPE) {
+                Type subType = Type.getType(factory.getSubType(member.getClazz()));
+                super.visitTypeInsn(Opcodes.NEW, subType.getInternalName());
+                super.visitInsn(Opcodes.DUP);
+                super.visitMethodInsn(Opcodes.INVOKESPECIAL,
+                        subType.getInternalName(),
+                        INIT,
+                        EMPTY_DESCRIPTOR,
+                        false);
+                return;
+            }
+            // Singleton and field
+            // Load container from provider
+            Method method = provider.getMethod();
+            super.visitMethodInsn(Opcodes.INVOKESTATIC,
+                    Type.getInternalName(provider.getType()),
+                    method.getName(),
+                    Type.getMethodDescriptor(method),
+                    false);
+            super.visitLdcInsn(Objects.hash(member.getValue(), member.getClazz()));
+            Class<?> container = method.getReturnType();
+            // Invoke necessary method from container
+            boolean isInterface = container.isInterface();
+            super.visitMethodInsn(isInterface ? Opcodes.INVOKEINTERFACE : Opcodes.INVOKEVIRTUAL,
+                    Type.getInternalName(container),
+                    policy == InjectPolicy.SINGLETON ? SINGLETON : VALUE,
+                    DESCRIPTOR,
+                    isInterface);
+            // Cast to expected type
+            super.visitTypeInsn(Opcodes.CHECKCAST, member.getType().getInternalName());
+        }
     }
 }
