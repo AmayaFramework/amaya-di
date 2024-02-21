d1f953133d1eb42875f172f6e3ffed9ef06047d8
RomanQed <gbakaldin@gmail.com>
2024-02-21 14:36:31 +0300

    Improve ArtifactProvider

--- src/main/java/io/github/amayaframework/di/ArtifactProvider.java
+++ src/main/java/io/github/amayaframework/di/ArtifactProvider.java
@@ -17,4 +17,16 @@ public interface ArtifactProvider extends Function<Artifact, Function0<Object>> 
      * @return null or {@link Function0} instance
      */
     Function0<Object> apply(Artifact artifact);
+
+    /**
+     * Gets the instantiator associated with the specified type.
+     *
+     * @param type the specified type
+     * @param <T>  service type
+     * @return null or {@link Function0} instance
+     */
+    @SuppressWarnings("unchecked")
+    default <T> Function0<T> apply(Class<T> type) {
+        return (Function0<T>) apply(new Artifact(type));
+    }
 }
