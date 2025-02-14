a40065ae7562641f3fe70ac51a339fa88fcc692a
Roman Bakaldin <gbakaldin@gmail.com>
2025-02-14 16:51:26 +0300

    Add module for reflect stub factory implementation

---
+++ amaya-di-reflect/src/main/java/io/github/amayaframework/di/reflect/ReflectStubFactory.java
@@ -0,0 +1,15 @@
+package io.github.amayaframework.di.reflect;
+
+import com.github.romanqed.jfunc.Function0;
+import io.github.amayaframework.di.scheme.ClassScheme;
+import io.github.amayaframework.di.stub.StubFactory;
+import io.github.amayaframework.di.stub.TypeProvider;
+
+public final class ReflectStubFactory implements StubFactory {
+
+    @Override
+    public Function0<?> create(ClassScheme scheme, TypeProvider provider) {
+        // TODO
+        return null;
+    }
+}
